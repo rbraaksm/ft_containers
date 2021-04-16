2100a914141e397df512a632709fb7fe77a8a39d
rbraaksm <renebraaksma@hotmail.com>
2021-04-16 12:56:31 +0200

    pointer fail

---
+++ vector/randomAccessIterator.hpp
@@ -0,0 +1,263 @@
+#ifndef RANDOMACCESSITERATOR_HPP
+#define RANDOMACCESSITERATOR_HPP
+
+#include <algorithm>
+
+namespace ft {
+
+	template<typename T, class Category = std::random_access_iterator_tag>
+	class randomAccessIterator {
+		public:
+			typedef T 						value_type;
+			typedef value_type& 			reference;
+			typedef value_type* 			pointer;
+			typedef size_t 					size_type;
+			typedef ptrdiff_t				difference_type;
+			typedef Category				iterator_category;
+			typedef randomAccessIterator	type;
+
+		protected:
+			pointer _ptr;
+
+		public:
+			randomAccessIterator() : _ptr(NULL) {}
+			explicit randomAccessIterator(pointer node) : _ptr(node) {}
+			randomAccessIterator(const randomAccessIterator& obj) {*this = obj;}
+			virtual ~randomAccessIterator() {}
+
+			randomAccessIterator& operator=(const randomAccessIterator& obj) {
+				if (&obj != this) {
+					_ptr = obj._ptr;
+				}
+				return (*this);
+			}
+
+			randomAccessIterator operator++(int) {
+				randomAccessIterator ret(*this);
+				_ptr++;
+				return (ret);
+			}
+
+			randomAccessIterator& operator++() { // Overload prefix ++
+				_ptr++;
+				return (*this);
+			}
+
+			randomAccessIterator operator+(difference_type n) {
+				randomAccessIterator ret(*this);
+				ret._ptr += n;
+				return (ret);
+			}
+
+			randomAccessIterator operator+=(difference_type n) {
+				_ptr += n;
+				return (*this);
+			}
+
+			randomAccessIterator operator--(int) { // Overload postfix -
+				randomAccessIterator ret(*this);
+				_ptr--;
+				return (ret);
+			}
+
+			randomAccessIterator& operator--() { // Overload prefix -
+				_ptr--;
+				return (*this);
+			}
+
+			randomAccessIterator operator-(difference_type n) {
+				randomAccessIterator ret(*this);
+				ret._ptr -= n;
+				return (ret);
+			}
+
+			difference_type operator-(randomAccessIterator it) {
+				int ret;
+				ret = _ptr - it.getPtr();
+				return (ret);
+			}
+
+			randomAccessIterator operator-=(difference_type n) {
+				_ptr -= n;
+				return (*this);
+			}
+
+			bool operator==(const randomAccessIterator& obj) {return (_ptr == obj._ptr);}
+			bool operator!=(const randomAccessIterator& obj) {return (_ptr != obj._ptr);}
+			bool operator>=(const randomAccessIterator& obj) {return (_ptr >= obj._ptr);}
+			bool operator<=(const randomAccessIterator& obj) {return (_ptr <= obj._ptr);}
+			bool operator>(const randomAccessIterator& obj) {return (_ptr > obj._ptr);}
+			bool operator<(const randomAccessIterator& obj) {return (_ptr < obj._ptr);}
+
+			reference operator*() {return (*_ptr);}
+			pointer operator->() {return (*(&(_ptr)));}
+			reference	operator[](difference_type n) {return (*(_ptr + n));}
+			pointer getPtr() const {return (_ptr);}
+	};
+
+	template<typename T, class Category = std::random_access_iterator_tag>
+	class constRandomAccessIterator: public randomAccessIterator<T> {
+	public:
+		typedef T 							value_type;
+		typedef value_type& 				reference;
+		typedef value_type* 				pointer;
+		typedef const value_type& 			const_reference;
+		typedef const value_type* 			const_pointer;
+		typedef size_t 						size_type;
+		typedef ptrdiff_t 					difference_type;
+		typedef Category 					iterator_category;
+		typedef constRandomAccessIterator 	type;
+
+		constRandomAccessIterator() : randomAccessIterator<T>(NULL) {}
+		explicit constRandomAccessIterator(pointer node) : randomAccessIterator<T>(node) {}
+		constRandomAccessIterator(const constRandomAccessIterator& obj) {*this = obj;}
+		constRandomAccessIterator(const randomAccessIterator<T>& obj) {*this = obj;}
+
+		constRandomAccessIterator& operator=(const constRandomAccessIterator& obj) {
+			if (&obj != this) {
+				this->_ptr = obj.getPtr();
+			}
+			return (*this);
+		}
+
+		constRandomAccessIterator& operator=(const randomAccessIterator<T>& obj) {
+			if (&obj != this) {
+				this->_ptr = obj.getPtr();
+			}
+			return (*this);
+		}
+
+		virtual ~constRandomAccessIterator() {}
+
+		const_reference operator*() {return (*this)->_ptr;}
+		const_pointer operator->() {return *(&(this->_ptr));}
+		const_reference	operator[](difference_type n) {return (*(this->_ptr + n));}
+	};
+
+	template<typename T, class Category = std::random_access_iterator_tag>
+	class revRandomAccessIterator {
+	public:
+		typedef T 						value_type;
+		typedef value_type& 			reference;
+		typedef value_type* 			pointer;
+		typedef size_t 					size_type;
+		typedef ptrdiff_t 				difference_type;
+		typedef Category 				iterator_category;
+		typedef revRandomAccessIterator type;
+
+	protected:
+		pointer _ptr;
+
+	public:
+		revRandomAccessIterator() : _ptr(NULL) {}
+		explicit revRandomAccessIterator(pointer node) : _ptr(node) {}
+		revRandomAccessIterator(const revRandomAccessIterator& obj) {*this = obj;}
+
+		revRandomAccessIterator& operator=(const revRandomAccessIterator& obj) {
+			if (&obj != this) {
+				_ptr = obj._ptr;
+			}
+			return (*this);
+		}
+
+		virtual ~revRandomAccessIterator() {}
+
+		bool operator==(const revRandomAccessIterator& obj) {return (_ptr == obj._ptr);}
+		bool operator!=(const revRandomAccessIterator& obj) {return (_ptr != obj._ptr);}
+
+		reference operator*() {return (*_ptr);}
+		pointer operator->() {return *(&(_ptr));}
+
+		revRandomAccessIterator operator++(int) { // Overload postfix ++
+			revRandomAccessIterator ret(*this);
+			_ptr--;
+			return (ret);
+		}
+
+		revRandomAccessIterator& operator++() { // Overload prefix ++
+			_ptr--;
+			return (*this);
+		}
+
+		revRandomAccessIterator operator+(difference_type n) {
+			revRandomAccessIterator ret(*this);
+			ret._ptr -= n;
+			return (ret);
+		}
+
+		revRandomAccessIterator operator+=(difference_type n) {
+			_ptr -= n;
+			return (_ptr);
+		}
+
+		revRandomAccessIterator operator--(int) { // Overload postfix -
+			revRandomAccessIterator ret(*this);
+			_ptr++;
+			return (ret);
+		}
+
+		revRandomAccessIterator& operator--() { // Overload prefix -
+			_ptr++;
+			return (*this);
+		}
+
+		revRandomAccessIterator operator-(difference_type n) {
+			revRandomAccessIterator ret(*this);
+			ret._ptr += n;
+			return (ret);
+		}
+
+		revRandomAccessIterator operator-=(difference_type n) {
+			_ptr += n;
+			return (_ptr);
+		}
+
+		bool operator>=(const revRandomAccessIterator& obj) {return (_ptr >= obj._ptr);}
+		bool operator<=(const revRandomAccessIterator& obj) {return (_ptr <= obj._ptr);}
+		bool operator>(const revRandomAccessIterator& obj) {return (_ptr > obj._ptr);}
+		bool operator<(const revRandomAccessIterator& obj) {return (_ptr < obj._ptr);}
+
+		reference	operator[](difference_type n) {return (*(_ptr + n));}
+		pointer getPtr() const {return (_ptr);}
+	};
+
+	template<typename T, class Category = std::random_access_iterator_tag>
+	class constRevRandomAccessIterator: public revRandomAccessIterator<T> {
+	public:
+		typedef T 								value_type;
+		typedef value_type& 					reference;
+		typedef value_type* 					pointer;
+		typedef const value_type& 				const_reference;
+		typedef const value_type*	 			const_pointer;
+		typedef size_t 							size_type;
+		typedef ptrdiff_t 						difference_type;
+		typedef Category 						iterator_category;
+		typedef constRevRandomAccessIterator	self_type;
+
+		constRevRandomAccessIterator() : revRandomAccessIterator<T>(NULL) {}
+		explicit constRevRandomAccessIterator(pointer node) : revRandomAccessIterator<T>(node) {}
+		constRevRandomAccessIterator(const constRevRandomAccessIterator& obj) {*this = obj;}
+		constRevRandomAccessIterator(const revRandomAccessIterator<T>& obj) {*this = obj;}
+		constRevRandomAccessIterator& operator=(const constRevRandomAccessIterator& obj) {
+			if (&obj != this) {
+				this->_ptr = obj.getPtr();
+			}
+			return (*this);
+		}
+
+		constRevRandomAccessIterator& operator=(const revRandomAccessIterator<T>& obj) {
+			if (&obj != this) {
+				this->_ptr = obj.getPtr();
+			}
+			return (*this);
+		}
+
+		virtual ~constRevRandomAccessIterator() {}
+		const_reference operator*() {return (*this)->_ptr;}
+		const_pointer operator->() {return *(&(this->_ptr));}
+		const_reference	operator[](difference_type n) {return (*(this->_ptr + n));}
+	};
+
+}
+
+#endif
---
+++ vector/vector.hpp
@@ -0,0 +1,254 @@
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+# include "../Traits.hpp"
+# include "randomAccessIterator.hpp"
+# include <memory>
+# include <iterator>
+# include <algorithm>
+
+namespace ft{
+    template < class T, class Alloc = std::allocator<T> >
+    class vector{
+        public:
+		// member types //
+            typedef T           	value_type;
+            typedef Alloc       	allocator_type;
+            typedef T          		&reference;
+            typedef const T     	&const_reference;
+            typedef T           	*pointer;
+            typedef const T     	*const_pointer;
+
+			typedef randomAccessIterator<value_type>			iterator;
+			typedef constRandomAccessIterator<value_type>		const_iterator;
+			typedef revRandomAccessIterator<value_type>			reverse_iterator;
+			typedef constRevRandomAccessIterator<value_type>	const_reverse_iterator;
+
+			typedef ptrdiff_t		difference_type;
+			typedef size_t			size_type;
+
+		private:
+			template <typename U>
+			void swap(U& x, U& y) {
+				U tmp = x;
+				x = y;
+				y = tmp;
+			}
+
+			allocator_type	_allocator;
+			pointer			_array;
+			size_type		_capacity;
+			size_type		_size;
+
+		public:
+		// member functions //
+		// Constructors
+		explicit vector(const allocator_type& alloc = allocator_type()) : _allocator(alloc), _array(NULL), _capacity(0), _size(0){}
+
+		explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
+				: _allocator(alloc), _array(NULL), _capacity(n), _size(n){
+			_array = new value_type[n];
+			for (size_type i = 0; i < n; i++)
+				_array[i] = val;
+		}
+
+		template <class InputIterator>
+        vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+				: _allocator(alloc){
+			_size = 0;
+			_capacity = first;
+			_array = new value_type[_capacity];
+			assign(first, last);
+		}
+
+		vector(const vector& x){
+			_size = 0;
+			_capacity = 0;
+			*this = x;
+		}
+
+		// Destructor
+		virtual ~vector(){
+			delete[] _array;
+		}
+
+		// Assignment operator
+		vector&	operator=(const vector& x){
+			if (this != &x){
+				if (_capacity){clear();}
+				_capacity = x._capacity;
+				_size = x._size;
+				_array = new value_type[_capacity];
+				assign(x.begin(), x.end());
+			}
+			return (*this);
+		}
+
+		// Iterators
+		iterator begin(){return (iterator(_array));}
+		const_iterator begin() const{return (const_iterator(_array));}
+		iterator end(){return (iterator(&_array[_size]));}
+		const_iterator end() const{return (const_iterator(&_array[_size]));}
+		reverse_iterator rbegin() {return (reverse_iterator(&_array[_size - 1]));}
+		const_reverse_iterator rbegin() const {return (const_reverse_iterator(&_array[_size - 1]));}
+		reverse_iterator rend() {return (reverse_iterator(_array-1));}
+		const_reverse_iterator rend() const {return (const_reverse_iterator(_array-1));}
+
+		// Capacity
+		size_type size() const{return (_size);}
+		size_type max_size() const{return (_allocator.max_size());}
+
+		void resize(size_type n, value_type val = value_type()){
+			while (n < _size)
+				pop_back();
+			while (n > _size)
+				push_back(val);
+		}
+
+		size_type capacity() const{return (_capacity);}
+
+		bool empty()const{
+			if (_size)
+				return (true);
+			return (false);
+		}
+
+		void reserve(size_type n){
+			if (n <= _capacity)
+				return ;
+			_capacity = (_capacity == 0 ? 1 : _capacity * 2);
+			pointer tmp = new value_type[_capacity];
+			for (size_type i = 0; i < _size; ++i)
+				tmp[i] = _array[i];
+			delete[] _array;
+			_array = tmp;
+		}
+
+		// Element access
+		reference operator[](size_type n){return (*(_array + n));}
+		const_reference operator[](size_type n) const{return (*(_array + n));}
+
+		reference at(size_type n){
+			if (n < _size)
+				return (*(_array + n));
+			throw (std::out_of_range("N out of range"));
+		}
+
+		const_reference at(size_type n) const{
+			if (n < _size)
+				return (*(_array + n));
+			throw (std::out_of_range("N out of range"));
+		}
+
+		reference front(){return (_array[0]);}
+		const_reference front() const{return (_array[0]);}
+		reference back(){return (_array[_size-1]);}
+		const_reference back() const{return (_array[_size-1]);}
+
+		// Modifiers
+		template<class InputIterator>
+		void assign(InputIterator first, InputIterator last,
+				typename ft::check_type<typename ft::iterator_traits<InputIterator>::iterator_category>::type* = 0) {
+			clear();
+			for (; first != last; ++first)
+				push_back(*first);
+		}
+
+		void assign(size_type n, const value_type& val){
+			clear();
+			for (; n > 0; n--)
+				push_back(val);
+		}
+
+		void push_back(const value_type& val){
+			if (_size == _capacity)
+				reserve(_size + 1);
+			_array[_size] = val;
+			++_size;
+		}
+
+		void pop_back(){
+			if (_size == 0)
+				return ;
+			_size--;
+			_allocator.destroy(_array + _size);
+		}
+
+		void clear(){
+			if (_size == 0)
+				return ;
+			for (; _size > 0; --_size)
+				_allocator.destroy(_array + _size);
+		}
+
+		iterator insert(iterator position, const value_type& val){
+			size_type n = position - begin();
+			insert(position, 1, val);
+			return (iterator(&_array[n]));
+		}
+
+		void insert(iterator position, size_type n, const value_type& val){
+			vector tmp;
+			iterator it = begin();
+
+			for (; it != position; ++it)
+				tmp.push_back(*it);
+			for (size_type i = 0; i < n; ++i)
+				tmp.push_back(val);
+
+			// iterator pos = position;
+			for (it = end(); position != it; ++position)
+				tmp.push_back(*position);
+			assign(tmp.begin(), tmp.end());
+		}
+
+		template <class InputIterator>
+		void insert(iterator position, InputIterator first, InputIterator last,
+				typename ft::check_type<typename ft::iterator_traits<InputIterator>::iterator_category>::type* = 0){
+			// reserve(_size + (last - first));
+			for (; first != last; ++first)
+				insert(position, *first);
+		}
+
+};
+
+// // Relational operators
+	template <class value_type, class allocator_type>
+	bool operator== (const vector<value_type,allocator_type>& lhs, const vector<value_type,allocator_type>& rhs) {
+		if (lhs.size() != rhs.size()) {
+			return false;
+		}
+		typename ft::vector<value_type>::iterator lhsIt = lhs.begin();
+		typename ft::vector<value_type>::iterator rhsIt = rhs.begin();
+		while (lhsIt != lhs.end() && rhsIt != rhs.end()) {
+			if (!(*lhsIt == *rhsIt)) {
+				return false;
+			}
+			++lhsIt;
+			++rhsIt;
+		}
+		return true;
+	}
+	template<class value_type, class allocator_type>
+	bool operator!=(const vector<value_type, allocator_type>& lhs, const vector<value_type, allocator_type>& rhs) {
+		return !(lhs == rhs);
+	}
+	template<class value_type, class allocator_type>
+	bool operator<(const vector<value_type, allocator_type>& lhs, const vector<value_type, allocator_type>& rhs) {
+		return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+	}
+	template<class value_type, class allocator_type>
+	bool operator<=(const vector<value_type, allocator_type>& lhs, const vector<value_type, allocator_type>& rhs) {
+		return !(rhs < lhs);
+	}
+	template<class value_type, class allocator_type>
+	bool operator>(const vector<value_type, allocator_type>& lhs, const vector<value_type, allocator_type>& rhs) {
+		return rhs < lhs; // Yes this is correct
+	}
+	template<class value_type, class allocator_type>
+	bool operator>=(const vector<value_type, allocator_type>& lhs, const vector<value_type, allocator_type>& rhs) {
+		return !(lhs < rhs); // Yes this is correct
+	}
+}
+
+#endif
