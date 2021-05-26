5de51858232d49e5862db1bc2c5f853c2056ed98
Rene Braaksma <rbraaksm@f0r5s7.codam.nl>
2021-05-26 11:37:31 +0200

    check

--- test/mapTest.cpp
+++ test/mapTest.cpp
@@ -6,7 +6,7 @@
 /*   By: renebraaksma <renebraaksma@student.42.f      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/01 11:14:55 by sam           #+#    #+#                 */
-/*   Updated: 2021/05/25 15:35:09 by rbraaksm      ########   odam.nl         */
+/*   Updated: 2021/05/26 09:37:28 by rbraaksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,190 +63,190 @@ TEST_CASE("Map: Constructors", "[Map]") {
 	REQUIRE(fifth['c'] == 50);
 }
 
-// TEST_CASE("Map: Insert + empty + size", "[Map]") {
-// 	ft::map<char,int> mymap;
-// 	REQUIRE(mymap.empty() == true);
-// 	// first insert function version (single parameter):
-// 	mymap.insert ( ft::pair<char,int>('a',100) );
-// 	mymap.insert ( ft::pair<char,int>('z',200) );
+TEST_CASE("Map: Insert + empty + size", "[Map]") {
+	ft::map<char,int> mymap;
+	REQUIRE(mymap.empty() == true);
+	// first insert function version (single parameter):
+	mymap.insert ( ft::pair<char,int>('a',100) );
+	mymap.insert ( ft::pair<char,int>('z',200) );
 
-// 	ft::pair<ft::map<char,int>::iterator,bool> ret;
-// 	ret = mymap.insert ( ft::pair<char,int>('z',500) );
-// 	REQUIRE(ret.second == false);
+	ft::pair<ft::map<char,int>::iterator,bool> ret;
+	ret = mymap.insert ( ft::pair<char,int>('z',500) );
+	REQUIRE(ret.second == false);
 
-// 	// second insert function version (with hint position):
-// 	ft::map<char,int>::iterator it = mymap.begin();
-// 	mymap.insert (it, ft::pair<char,int>('b',300));  // max efficiency inserting
-// 	mymap.insert (it, ft::pair<char,int>('c',400));	  // no max efficiency inserting
-// 	REQUIRE(mymap.size() == 4);
+	// second insert function version (with hint position):
+	ft::map<char,int>::iterator it = mymap.begin();
+	mymap.insert (it, ft::pair<char,int>('b',300));  // max efficiency inserting
+	mymap.insert (it, ft::pair<char,int>('c',400));	  // no max efficiency inserting
+	REQUIRE(mymap.size() == 4);
 
-// 	// third insert function version (range insertion):
-// 	ft::map<char,int> anothermap;
-// 	anothermap.insert(mymap.begin(), mymap.end());
-// 	REQUIRE(mymap.size() == 4);
-// }
+	// third insert function version (range insertion):
+	ft::map<char,int> anothermap;
+	anothermap.insert(mymap.begin(), mymap.end());
+	REQUIRE(mymap.size() == 4);
+}
 
-// TEST_CASE("Map: Find", "[Map]") {
-// 	ft::map<char,int> mymap;
-// 	// first insert function version (single parameter):
-// 	mymap.insert ( ft::pair<char,int>('a',100) );
-// 	mymap.insert ( ft::pair<char,int>('z',200) );
+TEST_CASE("Map: Find", "[Map]") {
+	ft::map<char,int> mymap;
+	// first insert function version (single parameter):
+	mymap.insert ( ft::pair<char,int>('a',100) );
+	mymap.insert ( ft::pair<char,int>('z',200) );
 
-// 	// second insert function version (with hint position):
-// 	ft::map<char,int>::iterator it;
-// 	it = mymap.find('a');
-// 	bool res = it == mymap.begin();
-// 	REQUIRE(res == true);
-// 	it = mymap.find('x');
-// 	res = it == mymap.end();
-// 	REQUIRE(res == true);
-// }
+	// second insert function version (with hint position):
+	ft::map<char,int>::iterator it;
+	it = mymap.find('a');
+	bool res = it == mymap.begin();
+	REQUIRE(res == true);
+	it = mymap.find('x');
+	res = it == mymap.end();
+	REQUIRE(res == true);
+}
 
-// TEST_CASE("Map: Iterators", "[Map]") {
-// 	ft::map<int, int> myMap;
+TEST_CASE("Map: Iterators", "[Map]") {
+	ft::map<int, int> myMap;
 
-// 	myMap.insert(ft::make_pair(9, 9));
-// 	myMap.insert(ft::make_pair(3, 3));
-// 	myMap.insert(ft::make_pair(8, 8));
-// 	myMap.insert(ft::make_pair(0, 0));
-// 	myMap.insert(ft::make_pair(7, 7));
-// 	myMap.insert(ft::make_pair(6, 6));
-// 	myMap.insert(ft::make_pair(5, 5));
-// 	myMap.insert(ft::make_pair(4, 4));
-// 	myMap.insert(ft::make_pair(2, 2));
-// 	myMap.insert(ft::make_pair(1, 1));
+	myMap.insert(ft::make_pair(9, 9));
+	myMap.insert(ft::make_pair(3, 3));
+	myMap.insert(ft::make_pair(8, 8));
+	myMap.insert(ft::make_pair(0, 0));
+	myMap.insert(ft::make_pair(7, 7));
+	myMap.insert(ft::make_pair(6, 6));
+	myMap.insert(ft::make_pair(5, 5));
+	myMap.insert(ft::make_pair(4, 4));
+	myMap.insert(ft::make_pair(2, 2));
+	myMap.insert(ft::make_pair(1, 1));
 
-// 	ft::map<int, int>::iterator it = myMap.begin();
-// 	for (int i = 0; i < 10; ++i) {
-// 		REQUIRE(i == it->second);
-// 		++it;
-// 	}
-// 	it = myMap.end();
-// 	--it;
-// 	for (int i = 9; i != 0; --i) {
-// 		REQUIRE(i == it->second);
-// 		--it;
-// 	}
-// 	ft::map<int, int>::reverse_iterator rit = myMap.rbegin();
-// 	for (int i = 9; i != 0; --i) {
-// 		REQUIRE(i == rit->second);
-// 		++rit;
-// 	}
-// 	rit = myMap.rend();
-// 	--rit;
-// 	for (int i = 0; i < 10; ++i) {
-// 		REQUIRE(i == rit->second);
-// 		--rit;
-// 	}
-// }
+	ft::map<int, int>::iterator it = myMap.begin();
+	for (int i = 0; i < 10; ++i) {
+		REQUIRE(i == it->second);
+		++it;
+	}
+	it = myMap.end();
+	--it;
+	for (int i = 9; i != 0; --i) {
+		REQUIRE(i == it->second);
+		--it;
+	}
+	ft::map<int, int>::reverse_iterator rit = myMap.rbegin();
+	for (int i = 9; i != 0; --i) {
+		REQUIRE(i == rit->second);
+		++rit;
+	}
+	rit = myMap.rend();
+	--rit;
+	for (int i = 0; i < 10; ++i) {
+		REQUIRE(i == rit->second);
+		--rit;
+	}
+}
 
-// TEST_CASE("Map: Erase", "[Map]") {
-// 	ft::map<char,int> mymap;
-// 	ft::map<char,int>::iterator it;
+TEST_CASE("Map: Erase", "[Map]") {
+	ft::map<char,int> mymap;
+	ft::map<char,int>::iterator it;
 
-// 	// insert some values:
-// 	mymap.insert(ft::make_pair('a', 10));
-// 	mymap.insert(ft::make_pair('b', 20));
-// 	mymap.insert(ft::make_pair('c', 30));
-// 	mymap.insert(ft::make_pair('d', 40));
-// 	mymap.insert(ft::make_pair('e', 50));
-// 	mymap.insert(ft::make_pair('f', 60));
+	// insert some values:
+	mymap.insert(ft::make_pair('a', 10));
+	mymap.insert(ft::make_pair('b', 20));
+	mymap.insert(ft::make_pair('c', 30));
+	mymap.insert(ft::make_pair('d', 40));
+	mymap.insert(ft::make_pair('e', 50));
+	mymap.insert(ft::make_pair('f', 60));
 
-// 	it=mymap.find('b');
-// 	mymap.erase (it);                   // erasing by iterator
+	it=mymap.find('b');
+	mymap.erase (it);                   // erasing by iterator
 
-// 	mymap.erase ('c');                  // erasing by key
+	mymap.erase ('c');                  // erasing by key
 
-// 	it=mymap.find ('e');
-// 	mymap.erase ( it, mymap.end() );    // erasing by range
+	it=mymap.find ('e');
+	mymap.erase ( it, mymap.end() );    // erasing by range
 
-// 	// show content:
-// 	it=mymap.begin();
-// 	REQUIRE(mymap.size() == 2);
-// 	REQUIRE(it->second == 10);
-// 	++it;
-// 	REQUIRE(it->second == 40);
-// }
+	// show content:
+	it=mymap.begin();
+	REQUIRE(mymap.size() == 2);
+	REQUIRE(it->second == 10);
+	++it;
+	REQUIRE(it->second == 40);
+}
 
-// TEST_CASE("Map: [] overload", "[Map]") {
-// 	ft::map<char,int> mymap;
+TEST_CASE("Map: [] overload", "[Map]") {
+	ft::map<char,int> mymap;
 
-// 	mymap['x'] = 100;
-// 	mymap['y'] = 200;
-// 	mymap['z'] = 300;
+	mymap['x'] = 100;
+	mymap['y'] = 200;
+	mymap['z'] = 300;
 
-// 	REQUIRE(mymap['x'] == 100);
-// 	REQUIRE(mymap['y'] == 200);
-// 	REQUIRE(mymap['z'] == 300);
-// }
+	REQUIRE(mymap['x'] == 100);
+	REQUIRE(mymap['y'] == 200);
+	REQUIRE(mymap['z'] == 300);
+}
 
-// TEST_CASE("Map: Swap", "[Map]") {
-// 	ft::map<char,int> foo,bar;
-// 	ft::map<char,int>::iterator it;
+TEST_CASE("Map: Swap", "[Map]") {
+	ft::map<char,int> foo,bar;
+	ft::map<char,int>::iterator it;
 
-// 	foo['x']=100;
-// 	foo['y']=200;
+	foo['x']=100;
+	foo['y']=200;
 
-// 	bar['a']=11;
-// 	bar['b']=22;
-// 	bar['c']=33;
+	bar['a']=11;
+	bar['b']=22;
+	bar['c']=33;
 
-// 	foo.swap(bar);
+	foo.swap(bar);
 
-// 	REQUIRE(foo.size() == 3);
-// 	REQUIRE(foo['a'] == 11);
-// 	REQUIRE(foo['b'] == 22);
-// 	REQUIRE(foo['c'] == 33);
-// 	it = foo.begin();
-// 	REQUIRE(it->second == 11);
+	REQUIRE(foo.size() == 3);
+	REQUIRE(foo['a'] == 11);
+	REQUIRE(foo['b'] == 22);
+	REQUIRE(foo['c'] == 33);
+	it = foo.begin();
+	REQUIRE(it->second == 11);
 
-// 	REQUIRE(bar.size() == 2);
-// 	REQUIRE(bar['x'] == 100);
-// 	REQUIRE(bar['y'] == 200);
-// 	it = bar.begin();
-// 	REQUIRE(it->second == 100);
-// }
+	REQUIRE(bar.size() == 2);
+	REQUIRE(bar['x'] == 100);
+	REQUIRE(bar['y'] == 200);
+	it = bar.begin();
+	REQUIRE(it->second == 100);
+}
 
-// TEST_CASE("Map: Count", "[Map]") {
-// 	ft::map<char,int> mymap;
+TEST_CASE("Map: Count", "[Map]") {
+	ft::map<char,int> mymap;
 
-// 	mymap['x'] = 100;
-// 	mymap['y'] = 200;
-// 	mymap['z'] = 300;
+	mymap['x'] = 100;
+	mymap['y'] = 200;
+	mymap['z'] = 300;
 
-// 	REQUIRE(mymap.count('x') == 1);
-// 	REQUIRE(mymap.count('a') == 0);
-// }
+	REQUIRE(mymap.count('x') == 1);
+	REQUIRE(mymap.count('a') == 0);
+}
 
-// TEST_CASE("Map: upper/lower bound", "[Map]") {
-// 	ft::map<char,int> mymap;
-// 	ft::map<char,int>::iterator itlow,itup;
+TEST_CASE("Map: upper/lower bound", "[Map]") {
+	ft::map<char,int> mymap;
+	ft::map<char,int>::iterator itlow,itup;
 
-// 	mymap['a']=20;
-// 	mymap['b']=40;
-// 	mymap['c']=60;
-// 	mymap['d']=80;
-// 	mymap['e']=100;
+	mymap['a']=20;
+	mymap['b']=40;
+	mymap['c']=60;
+	mymap['d']=80;
+	mymap['e']=100;
 
-// 	itlow=mymap.lower_bound ('b');  // itlow points to b
-// 	itup=mymap.upper_bound ('d');   // itup points to e (not d!)
-// 	REQUIRE(itlow->first == 'b');
-// 	REQUIRE(itup->first == 'e');
-// }
+	itlow=mymap.lower_bound ('b');  // itlow points to b
+	itup=mymap.upper_bound ('d');   // itup points to e (not d!)
+	REQUIRE(itlow->first == 'b');
+	REQUIRE(itup->first == 'e');
+}
 
-// TEST_CASE("Map: equal_range", "[Map]") {
-// 	ft::map<char,int> mymap;
+TEST_CASE("Map: equal_range", "[Map]") {
+	ft::map<char,int> mymap;
 
-// 	mymap['a']=10;
-// 	mymap['b']=20;
-// 	mymap['c']=30;
+	mymap['a']=10;
+	mymap['b']=20;
+	mymap['c']=30;
 
-// 	ft::pair<ft::map<char,int>::iterator,ft::map<char,int>::iterator> ret;
-// 	ret = mymap.equal_range('b');
+	ft::pair<ft::map<char,int>::iterator,ft::map<char,int>::iterator> ret;
+	ret = mymap.equal_range('b');
 
-// 	REQUIRE(ret.first->first == 'b');
-// 	REQUIRE(ret.second->first == 'c');
-// }
+	REQUIRE(ret.first->first == 'b');
+	REQUIRE(ret.second->first == 'c');
+}
 
 
 TEST_CASE("map-Empty container constructor", "[map]")
@@ -902,30 +902,30 @@ TEST_CASE("map-erase function version 2 erasing by key", "[map]")
 	REQUIRE(own.size() == real.size());
 	REQUIRE(own.empty() == real.empty());
 
-	// own.erase(6);
-	// real.erase(6);
+	own.erase(6);
+	real.erase(6);
 
 	REQUIRE(own.size() == real.size());
 	REQUIRE(own.empty() == real.empty());
-	// own.erase(10);
-	// real.erase(10);
+	own.erase(10);
+	real.erase(10);
 
-	// // Delete root with one left Child
-	// own.insert(ft::pair<int, int>(6, 8));
-	// real.insert(std::pair<int, int>(6, 8));
-	// own.insert(ft::pair<int, int>(4, 8));
-	// real.insert(std::pair<int, int>(4, 8));
+	// Delete root with one left Child
+	own.insert(ft::pair<int, int>(6, 8));
+	real.insert(std::pair<int, int>(6, 8));
+	own.insert(ft::pair<int, int>(4, 8));
+	real.insert(std::pair<int, int>(4, 8));
 
-	// REQUIRE(own.size() == real.size());
-	// REQUIRE(own.empty() == real.empty());
+	REQUIRE(own.size() == real.size());
+	REQUIRE(own.empty() == real.empty());
 
-	// own.erase(6);
-	// real.erase(6);
+	own.erase(6);
+	real.erase(6);
 
-	// REQUIRE(own.size() == real.size());
-	// REQUIRE(own.empty() == real.empty());
-	// own.erase(4);
-	// real.erase(4);
+	REQUIRE(own.size() == real.size());
+	REQUIRE(own.empty() == real.empty());
+	own.erase(4);
+	real.erase(4);
 
 	// // Delete Root with 2 children
 	// own.insert(ft::pair<int, int>(15, 15));
